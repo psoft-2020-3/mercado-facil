47743454f4bfaa18db6e88d13ccce11370cfeab7
fabiomorais <fabio.jorge@gmail.com>
2020-08-18 00:26:13 -0300

    Classe de teste base para o projeto

---
+++ src/test/java/com/ufcg/psoft/estoquefacil/EstoquefacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufcg.psoft.estoquefacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoquefacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
