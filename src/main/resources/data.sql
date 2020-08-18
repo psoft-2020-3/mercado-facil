5e8b61e6c8f9af7aa5d11ce831e948d7e055fd8b
fabiomorais <fabio.jorge@gmail.com>
2020-08-18 00:28:31 -0300

    Arquivo sql executado automaticamente para adicionar exemplos no banco

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+insert into produto
+values(10001,'Leite Integral', '87654321-B', 'Parmalat', 'Mercearia', 4.5, 1);
+insert into produto
+values(10002,'Arroz Integral', '87654322-B', 'Tio Joao', 'Perecíveis', 5.5, 1);
+insert into produto
+values(10003,'Sabao em Po', '87654323-B', 'OMO', 'Limpeza', 12, 1);
+insert into produto
+values(10004,'Agua Sanitaria', '87654324-C', 'Dragao', 'limpesa', 3, 1);
+insert into produto
+values(10005,'Creme Dental', '87654325-C', 'Colgate', 'HIGIENE', 2.5, 1);
